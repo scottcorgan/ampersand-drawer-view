e93e945d0f017d86c52e3198db3c3e4132cf9963
Scott Corgan <scottcorgan@gmail.com>
2014-11-12 12:46:34 -0800

    show/hide drawer on resize

--- ampersand-drawer-view.js
+++ ampersand-drawer-view.js
@@ -2,17 +2,34 @@ var _ = require('underscore');
 var View = require('ampersand-view');
 var defaultTemplate = require('./lib/default-template');
 var style = require('./lib/outfit');
+var outerWidth = require('outerwidth');
+var trigger = require('./lib/trigger');
+var prefixedCalc = require('./lib/prefixed-calc');
 
 module.exports = View.extend({
   props: {
-    template: ['string', true, defaultTemplate],
+    template: ['string', true, defaultTemplate()],
     drawerWidth: ['number', false, 256],
+    responsiveWidth: ['number', false, 640],
+    rightDrawer: ['boolean', false, false],
+    withHeader: ['boolean', false, false], // TODO: test this next!
+    defaultNarrowClass: ['string', false , 'narrow'],
     main: 'object',
     drawer: 'object',
     toggle: 'object'
   },
   
   initialize: function () {
+    
+    // Resize listeners
+    var resizeHandler = _.bind(this._handleWindowResize, this);
+    window.addEventListener('resize', resizeHandler);
+    this.once('remove', function () {
+      
+      // TODO: test this
+      window.removeEventListener('resize', resizeHandler);
+    });
+    this.listenTo(this, 'resize', resizeHandler);
     
     this.render();
     
@@ -24,36 +41,49 @@ module.exports = View.extend({
     this.renderWithTemplate();
     
     // Query elements
-    this.main = this.queryByHook('main');
     this.drawer = this.queryByHook('drawer');
+    this.main = this.queryByHook('main');
     this.toggle = this.queryByHook('toggle');
     
-    this._defaultStyles();
+    this._setDefaultStyles();
+    
+    _.defer(_.bind(function () {
+      this.trigger('resize');
+    }, this));
     
     return this;
   },
   
-  _defaultStyles: function () {
-    
-    // Set element styles
-    style(this.el, {
-      height: '100%',
-      overflow: 'hidden', // TODO: write test
-      width: '100%'
-    });
+  _setDefaultStyles: function () {
     
     style(this.drawer, {
-      height: '100%',
-      float: 'left',
-      overflow: 'auto',
       width: this.drawerWidth + 'px'
     });
     
     style(this.main, {
-      height: '100%',
-      float: 'left',
-      overflow: 'auto',
-      width: 'calc(100% - ' + this.drawerWidth + 'px)'
+      width: prefixedCalc('100% - ' + this.drawerWidth + 'px')
     });
-  }
+  },
+  
+  _handleWindowResize: function (e) {
+    
+    if (this.el && outerWidth(this.el) <= this.responsiveWidth) {
+      
+      // Hide the drawer when it's less than the response width
+      this.el.classList.add(this.defaultNarrowClass);
+      
+      style(this.main, {
+        width: '100%'
+      });
+    }
+    else {
+      
+      // Show the drawer when it's more than the response width
+      this.el.classList.remove(this.defaultNarrowClass);
+      
+      style(this.main, {
+        width: prefixedCalc('100% - ' + this.drawerWidth + 'px')
+      });
+    }
+  },
 });
--- test/index.js
+++ test/index.js
@@ -1,9 +1,15 @@
+var _ = require('underscore');
 var test = require('tape');
 var DrawerView = require('../ampersand-drawer-view');
 var View = require('ampersand-view');
 var outerWidth = require('outerwidth');
 var outerHeight = require('../lib/outerheight');
 var rawStyle = require('../lib/raw-style');
+var trigger = require('../lib/trigger');
+var prefix = require('../lib/prefix');
+
+var DRAWER_VISIBLE_MATRIX = 'matrix(1, 0, 0, 1, 0, 0)';
+var DRAWER_HIDDEN_MATRIX = 'matrix(1, 0, 0, 1, -256, 0)';
 
 // Set up body,html
 var html = document.querySelector('html');
@@ -19,7 +25,6 @@ document.body.style.padding = '0';
 test('default layout', function (t) {
   
   var drawerView = new DrawerView({});
-  
   document.body.appendChild(drawerView.el);
   
   // Elements
@@ -40,37 +45,93 @@ test('default layout', function (t) {
   t.equal(rawStyle(drawerView.el, 'overflow'), 'hidden', 'el overflow');
   t.equal(rawStyle(drawerView.drawer, 'overflow'), 'auto', 'drawer overflow');
   t.equal(rawStyle(drawerView.main, 'overflow'), 'auto', 'main overflow');
-  t.equal(rawStyle(drawerView.drawer, 'float'), 'left', 'drawer float');
-  t.equal(rawStyle(drawerView.main, 'float'), 'left', 'main float');
+  t.equal(rawStyle(drawerView.drawer, 'position'), 'absolute', 'position absolute');
+  t.equal(rawStyle(drawerView.drawer, 'left'), '0px', 'position location left');
+  t.equal(rawStyle(drawerView.drawer, 'top'), '0px', 'position location top');
+  t.equal(rawStyle(drawerView.main, 'float'), 'right', 'main float');
   
-  document.body.innerHTML = '';
+  drawerView.remove();
   t.end();
 });
 
-
-
-// test('testing', function (t) {
+test('custom drawer width', function (t) {
   
-//   var MainView = View.extend({
-//     template: '<div></div>',
+  var drawerView = new DrawerView({
+    drawerWidth: 300
+  });
+  document.body.appendChild(drawerView.el);
+  
+  t.equal(outerWidth(drawerView.drawer), 300, 'default drawer width');
+  
+  drawerView.remove();
+  t.end();
+});
+
+test('custom responsive width', function (t) {
+  
+  var drawerView = new DrawerView({
+    responsiveWidth: 1200
+  });
+  
+  document.body.appendChild(drawerView.el);
+  
+  setTimeout(function () {
     
-//     render: function () {
+    t.equal(rawStyle(drawerView.drawer, prefix.css + 'transform'), DRAWER_HIDDEN_MATRIX, 'drawer is hidden');
+    t.equal(rawStyle(drawerView.main, 'width'), outerWidth(drawerView.el) + 'px', 'main container full width');
+    
+    drawerView.remove();
+    t.end();
+  }, 10);
+  
+});
+
+test('drawer hides when responsive width threshold is crossed', function (t) {
+  
+  var drawerView = new DrawerView({
+    responsiveWidth: 600
+  });
+  
+  document.body.appendChild(drawerView.el);
+  
+  setTimeout(function () {
+    
+    // Drawer visible
+    t.equal(rawStyle(drawerView.drawer, prefix.css + 'transform'), DRAWER_VISIBLE_MATRIX, 'drawer is visible');
+    t.ok(outerWidth(drawerView.main) < outerWidth(drawerView.el), 'main container partial width');
+    
+    document.body.style.width = "400px";
+    trigger(window, 'resize');
+    
+    _.defer(function () {
       
-//       this.renderWithTemplate();
+      // Drawer hidden
+      t.equal(rawStyle(drawerView.drawer, prefix.css + 'transform'), DRAWER_HIDDEN_MATRIX, 'drawer is hidden');
+      t.equal(rawStyle(drawerView.main, 'width'), outerWidth(drawerView.el) + 'px', 'main container full width');
       
-//       var drawer = new DrawerView({
+      document.body.style.width = "1000px";
+      trigger(window, 'resize');
+      
+      _.defer(function () {
         
-//       });
-//     }
-//   });
-  
-//   var mainView = new MainView();
-//   document.body.appendChild(mainView.render().el);
-  
-//   t.end();
-// });
+        // Drawer visible
+        t.equal(rawStyle(drawerView.drawer, prefix.css + 'transform'), DRAWER_VISIBLE_MATRIX, 'drawer is visible');
+        // TODO: phantomjs does not support calc(), but this test passes in browser
+        // t.ok(outerWidth(drawerView.main) < outerWidth(drawerView.el), 'main container full width');
+        
+        drawerView.remove();
+        t.end();
+      });
+    });
+  }, 10);
+});
 
-
+test('renders element as narrow', function (t) {
+  
+  
+  
+  t.end();
+});
 
 // test('replaces default elements with elements defined with data-hook', function (t) {
   
